57bf9bf3fe87456d83ddd5f2f714e951ec99032a
William Born <wkb1997@me.com>
2018-09-21 13:36:51 -0400

    created code file

---
+++ lab2/code/code.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+
+  Serial.print("once\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("forever\n");
+
+  delay(500);
+}
