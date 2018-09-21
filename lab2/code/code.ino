b37c50f11efe850ac24d6d8c1689f13664d3a2d2
William Born <wkb1997@me.com>
2018-09-21 13:46:07 -0400

    started code

--- lab2/code/code.ino
+++ lab2/code/code.ino
@@ -1,3 +1,20 @@
+/*
+LAB 2
+XE472
+CDT Born '19 H-2 and CDT Belardo-Flowers '19 G-2
+DUE At Start of Lab 3 / 1 OCT 2018
+
+TODO:
+1) set up robot hardware
+2) define sensors
+3) design state machine
+4) interface w/ servos
+5) ???
+6) Profit
+
+
+*/
+
 void setup() {
   Serial.begin(9600);
 
@@ -9,4 +26,43 @@ void loop() {
   Serial.print("forever\n");
 
   delay(500);
+
+  sensors();
+  state_machine(set_flags());
 }
+
+int set_flags(){
+  
+}
+
+void state_machine(int state)
+{
+  switch(state){
+    case 0:
+      //TODO
+    break;
+    case 1:
+      //TODO
+    break;
+    case 2:
+      //TODO
+    break;
+    case 3:
+      //TODO
+    break;
+    case 4:
+      //TODO
+    break;
+    case 5:
+      //TODO
+    break;
+    case 6:
+      //TODO
+    break;
+    case 7:
+      //TODO
+    break;
+    
+  }
+}
+
